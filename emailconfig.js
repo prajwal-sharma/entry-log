da543dcce23c0447c97233326b92b04cf59c9e11
prajwal-sharma <sharmaprajwal@gmail.com>
2021-09-24 00:41:40 +0530

    'changes'

---
+++ emailconfig.js
@@ -0,0 +1,5 @@
+module.exports.host = 'smtp.gmail.com'
+module.exports.isSecure = true
+module.exports.email = 'noreplyemail357@gmail.com'
+module.exports.password = 'PiT4Ycap6qFUigg'
+module.exports.from = 'security@company.com'
